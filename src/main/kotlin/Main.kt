ff89c7944b9ffbb2d96f5bd604653bddad56389e
songtomtom <tom.song@luxrobo.com>
2022-07-16 16:52:57 +0900

    펏그트 커밋

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    val one = 1 // Int
+    val threeBillion = 3000000000 // Long
+    val oneLong = 1L // Long
+    val oneByte: Byte = 1
+
+    println("Program arguments: ${args.joinToString()}")
+}
